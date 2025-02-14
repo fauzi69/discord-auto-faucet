9e2205fb4ce1ec09a324c24d91f63c693a2865d2
fauzi <36311071+fauzi69@users.noreply.github.com>
2025-02-14 21:56:41 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# Simple Discord faucetbot
+# Created By Viloid ( github.com/vsec7 ) modified by fauzi69
+# Use At Your Own Risk
+
+import requests, random, sys, yaml, time
+
+class Discord:
+
+    def __init__(self, t):
+        self.base = "https://discord.com/api/v9"
+        self.auth = { 'authorization': t }
+        
+    def getMe(self):
+        u = requests.get(self.base + "/users/@me", headers=self.auth).json()
+        return u
+        
+    def getMessage(self, cid, l):
+        u = requests.get(self.base + "/channels/" + str(cid) + "/messages?limit=" + str(l), headers=self.auth).json()
+        return u
+        
+    def sendMessage(self, cid, txt):    
+        u = requests.post(self.base + "/channels/" + str(cid) + "/messages", headers=self.auth, json={ 'content': txt }).json()
+        return u
+
+    def replyMessage(self, cid, mid, txt):    
+        u = requests.post(self.base + "/channels/" + str(cid) + "/messages", headers=self.auth, json={ 'content': txt, 'message_reference': { 'message_id': str(mid) } }).json()
+        return u
+
+    def deleteMessage(self, cid, mid):
+        u = requests.delete(self.base + "/channels/" + str(cid) + "/messages/" + str(mid), headers=self.auth)
+        return u
+
+def main():
+    with open('config.yaml') as cfg:
+        conf = yaml.load(cfg, Loader=yaml.FullLoader)
+
+    if not conf['BOT_TOKEN']:
+        print("[!] Please provide discord token at config.yaml!")
+        sys.exit()
+
+    if not conf['CHANNEL_ID']:
+        print("[!] Please provide channel id at config.yaml!")
+        sys.exit()
+
+    delay = conf['DELAY']
+
+    while True:
+        for token_index, token in enumerate(conf['BOT_TOKEN']):
+            try:
+                for chan in conf['CHANNEL_ID']:
+                    Bot = Discord(token)
+                    me = Bot.getMe()['username'] + "#" + Bot.getMe()['discriminator']
+                    
+                    with open("faucet.txt", "r") as file:
+                        faucet_lines = file.readlines()
+                    
+                    if faucet_lines:  # Pastikan ada isi dalam faucet.txt
+                        # Ambil pesan sesuai index token
+                        if token_index < len(faucet_lines):
+                            message = faucet_lines[token_index].strip()
+                            send = Bot.sendMessage(chan, message)
+                            print("[{}][{}][FAUCET] {}".format(me, chan, message))            
+
+            except Exception as e:
+                print(f"[Error] {token} : {str(e)}")
+
+        print("-------[ Delay for {} seconds ]-------".format(delay))
+        time.sleep(delay)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as err:
+        print(f"{type(err).__name__} : {err}")
